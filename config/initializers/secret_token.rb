1c2b1203175370e864939fb2cf55bc37840291a5
Eric Nordmeyer <eric@nordmeyer.name>
2011-07-06 13:20:43 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bb2011::Application.config.secret_token = '8394151639d8cf5e75d00e211349a960f9e5d21e32da237c1429ece2b0d9a2956345cd32a779c0caeea406b3e985c354bfa836819cb594d339edfdfa2057093f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Bb2011::Application.config.session_store :cookie_store, key: '_bb2011_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Bb2011::Application.config.session_store :active_record_store
